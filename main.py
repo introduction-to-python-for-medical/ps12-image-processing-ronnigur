bb7056ea2f6e202d44b6a6062c1102c298da9b23
ronnigur <rgwr3719@gmail.com>
2025-01-29 22:32:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+def save_image(img, path):
+    img = Image.fromarray(img)
+    img.save(path)
+
+path_to_image = "./image.jpg"
+
+# Load the image
+img = load_image(path_to_image)
+
+# Perform edge detection
+edges = edge_detection(img)
+
+path_to_save = "./output.png"
+# Save the image
+save_image(edges, path_to_save)
